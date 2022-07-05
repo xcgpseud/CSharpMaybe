2b8678960114871a459169bf5f73802b865ac7f3
xcgpseud <chris.evans92@hotmail.com>
2022-07-05 21:01:38 +0100

    Add nothing tests

--- Maybe.Tests/Maybe.Tests/Tests/NothingTests.cs
+++ Maybe.Tests/Maybe.Tests/Tests/NothingTests.cs
@@ -1,6 +1,46 @@
-﻿namespace Maybe.Tests.Tests;
+﻿using System;
+using AutoFixture;
+using FluentAssertions;
+using Maybe.Exceptions;
+using Maybe.Tests.FakeStuff.Entities;
+using Maybe.Tests.FakeStuff.Repositories;
+using NUnit.Framework;
 
-public class NothingTests
+namespace Maybe.Tests.Tests;
+
+public class NothingTests : MaybeTestBase
 {
-    
+    private PersonRepository _repository;
+
+    [SetUp]
+    public void LocalSetUp()
+    {
+        _repository = new PersonRepository();
+    }
+
+    [Test]
+    public void Maybe_ReceivesNull_IsValidNothing()
+    {
+        var exceptionMessage = "Derpity derp, y'failed.";
+
+        var defaultPersonEntity = Fixture.Create<PersonEntity>();
+        
+        var repositoryResult = _repository.Get(Guid.NewGuid());
+
+        repositoryResult.Should().BeNull();
+
+        var sut = Maybe<PersonEntity>.From(repositoryResult);
+
+        sut.Should().BeOfType<Nothing<PersonEntity>>();
+
+        sut.Invoking(x => x.Get()).Should().Throw<MaybeValueNotFoundException>();
+        sut.GetOrCall(() => defaultPersonEntity).Should().Be(defaultPersonEntity);
+        sut.GetOrElse(defaultPersonEntity).Should().Be(defaultPersonEntity);
+        sut.Invoking(x => x.GetOrThrow(new Exception(exceptionMessage)))
+            .Should().Throw<Exception>()
+            .WithMessage(exceptionMessage);
+
+        sut.IsEmpty().Should().BeTrue();
+        sut.IsNotEmpty().Should().BeFalse();
+    }
 }
